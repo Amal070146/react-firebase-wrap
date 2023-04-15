aad2546ad06e3d60b17b03c0b5812b59220eb1fe
Amal C P <acp070146@gmail.com>
2023-04-15 15:07:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Reset from "./pages/Reset/Reset";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/reset" element={<Reset />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
